e18bec27c7ac77e071f7bfcd15475c4398a542c6
Chris Burnell <me@chrisburnell.com>
2015-04-30 14:25:30 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+/**
+ * Flyknife Comics gulp Configuration
+ */
+
+// Define gulp objects
+var gulp         = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss    = require('gulp-minify-css'),
+    plumber      = require('gulp-plumber'),
+    notify       = require('gulp-notify'),
+    rename       = require('gulp-rename'),
+    sass         = require('gulp-sass'),
+    watch        = require('gulp-watch');
+
+// Define the locations of our assets
+var cssPath = 'css/';
+
+// -----------------------------------------------------------------------------
+
+// Compile SASS, autoprefix, generate sourcemaps, and minify
+gulp.task('css', function() {
+    return gulp.src(cssPath + 'flyknife.scss')
+        .pipe(plumber())
+        .pipe(sass({
+            errLogToConsole: true,
+            style: 'expanded'
+        }))
+        .pipe(autoprefixer('last 2 versions', '> 1%'))
+        .pipe(gulp.dest(cssPath))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(minifycss())
+        .pipe(gulp.dest(cssPath))
+        .pipe(notify({ title: 'gulp', message: 'CSS compiled.', onLast: true }));
+});
+
+// -----------------------------------------------------------------------------
+
+// Default task
+gulp.task('default', function() {
+    gulp.start('css');
+});
+
+// -----------------------------------------------------------------------------
+
+// Watch files and perform the appropriate tasks
+gulp.task('watch', ['css'], function() {
+    watch(cssPath + '**/*.scss', function() {
+        gulp.start('css');
+    });
+});
