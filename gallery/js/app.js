c9278803905822b3ab10444854bd25dd1a4173fc
Bruce Delo <bruce.delo@gmail.com>
2016-10-18 12:51:02 -0300

    Update app.js

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -18,6 +18,6 @@ galleryApp.directive('currentImage', function() {
     console.log("Current Image is present.");
     return {
         restrict: 'E',
-        templateUrl: "currentImage.html"
+        templateUrl: "/js/currentImage.html"
     }
 });
