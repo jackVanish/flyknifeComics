ae30b716a86cdf0dfaaa6c33be68372ce964fc9a
Bruce Delo <bruce.delo@gmail.com>
2016-10-18 11:39:16 -0300

    Update app.js
    Re-added directive.

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -3,3 +3,10 @@ var galleryApp = angular.module("bookGallery", []);
 galleryApp.controller('galleryCtrl', ['$scope', function($scope) {
     $scope.test = "Welcome to the Gallery";
   }]);
+
+galleryApp.directive('currentPage', function() {
+    console.log("Current Page is present.");
+    return {
+        restrict: 'E';
+    }
+});
