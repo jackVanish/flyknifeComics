cdcdc7c48e29d4bd37c942d98bc32897994061f3
Bruce Delo <bruce.delo@gmail.com>
2016-10-18 11:29:06 -0300

    Update app.js
    Added controller.

--- gallery/js/app.js
+++ gallery/js/app.js
@@ -1,4 +1,5 @@
 angular.module("bookGallery", [])
   .directive('currentImage', function() {
     console.log("Current Image is on the page.");
-  });
+  })
+  .controller('galleryCtrl', ['$scope', function($scope) {}];
